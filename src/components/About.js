b80643a37203aa1a810ec7a650ce73b92044c25c
Akshay Ram Chavan <111384131+akshaychavan23031998@users.noreply.github.com>
2024-04-11 11:09:12 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import react from "react";
+const About = () => {
+    return (
+       <div className="about">
+            <h1> About </h1>
+            <h2> Namaste React by Akshay Saini </h2>
+       </div>
+    )
+}
+
+export default About;
